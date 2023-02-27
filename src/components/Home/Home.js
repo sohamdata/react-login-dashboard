198f87c642750fc387e13639012f3a797305b9e7
Soham Datta <sohamdatta34@gmail.com>
2023-02-27 21:48:08 +0530

    added logout screen to dashboard

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,3 +1,4 @@
+import Button from '../UI/Button/Button';
 import Card from '../UI/Card/Card';
 import classes from './Home.module.css';
 
@@ -5,6 +6,7 @@ const Home = (props) => {
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
+      <Button onClick={props.onLogout}>Logout</Button>
     </Card>
   );
 };
---
+++ src/store/auth-context.js
@@ -0,0 +1,5 @@
+const AuthContext = React.createContext({
+    isLoggedIn: false,
+});
+
+export default AuthContext;
