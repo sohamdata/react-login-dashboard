1716baf428493ba75217207362277b96f9254dce
Soham Datta <sohamdatta34@gmail.com>
2023-03-13 21:51:35 +0530

    pallete

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -4,7 +4,7 @@ import classes from './MainHeader.module.css';
 const MainHeader = (props) => {
 	return (
 		<header className={classes['main-header']}>
-			<h1>A Typical Page</h1>
+			<h1>atypical</h1>
 			<Navigation />
 		</header>
 	);
